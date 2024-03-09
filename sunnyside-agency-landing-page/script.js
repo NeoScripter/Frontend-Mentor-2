d26c931f7ffb7028bc15950bdb31aedd1d3db909
Ilya <sange0337@gmail.com>
2024-03-09 17:09:34 +0800

    Made the sections interactive and fixed the bug related to shifted page's position

--- sunnyside-agency-landing-page/script.js
+++ sunnyside-agency-landing-page/script.js
@@ -1,10 +1,47 @@
-function setFillColor(element, color) {
-    element.style.fill = color;
+/* Making share links on the bottom white when the cursor is above them */
+
+document.querySelectorAll('.black').forEach(element => {
+    element.addEventListener('mouseover', () => toggleRelatedDisplay(element, true));
+    element.addEventListener('focus', () => toggleRelatedDisplay(element, true));
+    
+    element.addEventListener('mouseout', () => toggleRelatedDisplay(element, false));
+    element.addEventListener('blur', () => toggleRelatedDisplay(element, false));
+  });
+  
+  function toggleRelatedDisplay(element, showWhite) {
+    const relatedWhite = element.nextElementSibling;
+    if (showWhite) {
+      element.style.display = 'none';
+      if (relatedWhite && relatedWhite.classList.contains('white')) {
+        relatedWhite.style.display = 'block';
+      }
+    } else {
+      element.style.display = 'block';
+      if (relatedWhite && relatedWhite.classList.contains('white')) {
+        relatedWhite.style.display = 'none';
+      }
+    }
   }
   
-  document.querySelectorAll('.black').forEach(element => {
-    element.addEventListener('mouseover', () => setFillColor(element, 'white'));
-    element.addEventListener('mouseout', () => setFillColor(element, 'black'));
-    element.addEventListener('focus', () => setFillColor(element, 'white'));
-    element.addEventListener('blur', () => setFillColor(element, 'black'));
+
+/* Making layout sections appear smoothly one by one */
+
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('translated');
+          entry.target.classList.add('visible');
+        } else {
+          entry.target.classList.remove('visible');
+        }
+      });
+    }, {
+      rootMargin: '0px',
+      threshold: 0.1
+    });
+  
+    document.querySelectorAll('.prehidden').forEach(div => {
+      observer.observe(div);
+    });
   });
